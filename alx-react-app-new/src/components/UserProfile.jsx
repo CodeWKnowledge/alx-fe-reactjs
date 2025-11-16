b2518d10cd97d08cc478201a70ee8794d653e6d9
Knowledge <udohknowledge5@gmail.com>
2025-11-16 01:25:10 +0100

    updates

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -20,10 +20,11 @@ const UserProfile = (props) => {
           textAlign: "center",
           padding: "30px",
           borderRadius: "25px",
-          color: "#fff",
+          color: "blue",
           width: "50%",
           boxShadow:
             "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px, rgba(0, 0, 0, 0.56) 0px 22px 70px 4px",
+            border: '1px solid gray',  margin: '10px'
         }}
       >
         <h2>{props.name}</h2>
@@ -32,6 +33,7 @@ const UserProfile = (props) => {
       </div>
     </div>
   );
+
 };
 
 export default UserProfile;
