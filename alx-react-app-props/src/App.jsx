94c17b3b6fba2e428f71875640e05ee2c1da41af
Knowledge <udohknowledge5@gmail.com>
2025-11-16 10:15:22 +0100

    test

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ProfilePage from '../../alx-react-app-props/src/ProfilePage';
-import UserContext from './UserContext';
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext';
 
 function App() {
   const [userData, setUserData] = React.useState({ name: "Jane Doe", email: "jane.doe@example.com" });
