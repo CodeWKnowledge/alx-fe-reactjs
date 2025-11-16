a30f8f8e963cde711f08398a2ecc2410e46253a6
Knowledge <udohknowledge5@gmail.com>
2025-11-16 10:19:18 +0100

    Arghhh

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,13 +3,13 @@ import ProfilePage from './components/ProfilePage';
 import UserContext from './components/UserContext';
 
 function App() {
-  const [userData, setUserData] = React.useState({ name: "Jane Doe", email: "jane.doe@example.com" });
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return(
-    <UserContext.Provider value={{ userData, setUserData }}>
-      <ProfilePage userData={userData} />
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
     </UserContext.Provider>
-  )
+  );
 }
 
 export default App;
